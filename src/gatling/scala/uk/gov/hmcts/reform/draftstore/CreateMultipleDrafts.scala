8736f3b3bf4c7226baf86cddf465826e72e6bc75
Luigi Bitonti <luigi.bitonti@hmcts.net>
2019-04-16 23:45:45 +0100

    Use 3000 users scenario

--- src/gatling/scala/uk/gov/hmcts/reform/draftstore/CreateMultipleDrafts.scala
+++ src/gatling/scala/uk/gov/hmcts/reform/draftstore/CreateMultipleDrafts.scala
@@ -8,7 +8,6 @@ import uk.gov.hmcts.reform.draftstore.actions.DeleteAll.deleteAll
 import uk.gov.hmcts.reform.draftstore.actions.ReadOne.readOne
 import uk.gov.hmcts.reform.draftstore.actions.setup.Idam
 import uk.gov.hmcts.reform.draftstore.actions.setup.LeaseServiceToken.leaseServiceToken
-import uk.gov.hmcts.reform.draftstore.utils.{IdamUserHolder, User}
 
 import scala.concurrent.duration._
 
@@ -21,28 +20,9 @@ class CreateMultipleDrafts extends Simulation {
       .baseURL(config.getString("baseUrl"))
       .contentTypeHeader("application/json")
 
-//  val registerAndSignIn =
-//    scenario("Register and sign in")
-//      .exec(Idam.registerAndSignIn)
-//      .exec(session => {
-//        IdamUserHolder.push(User(session("email").as[String], session("user_token").as[String]))
-//        session
-//      })
 
   val createAndReadDrafts =
     scenario("Register and create multiple drafts")
-//      .feed(
-//        Iterator.continually(
-//          IdamUserHolder
-//            .pop()
-//            .map(user =>
-//              Map(
-//                "email" -> user.email,
-//                "user_token" -> user.token
-//              )
-//            )
-//        ).takeWhile(_.nonEmpty).flatten
-//      )
       .exec(Idam.registerAndSignIn)
       .pause(2 second, 5 seconds)
       .exec(session => {
@@ -65,14 +45,10 @@ class CreateMultipleDrafts extends Simulation {
 
   setUp(
     // Load test over 1 hour - settings
-    createAndReadDrafts.inject(rampUsers(10).over(1.minutes))
-    //registerAndSignIn.inject(rampUsers(3000).over(60.minutes)),
-    //createAndReadDrafts.inject(nothingFor(3.minute), rampUsers(3000).over(60.minutes))
+    createAndReadDrafts.inject(rampUsers(3000).over(60.minutes))
     // Regression (pipeline) - settings
-    //registerAndSignIn.inject(rampUsers(100).over(10.seconds)),
-    //createAndReadDrafts.inject(nothingFor(30.seconds), rampUsers(100).over(5.seconds))
+    //createAndReadDrafts.inject(rampUsers(300).over(360.seconds)),
     // Single user
-    //registerAndSignIn.inject(rampUsers(1).over(1.seconds)),
-    //createAndReadDrafts.inject(nothingFor(3.seconds), rampUsers(1).over(1.seconds))
+    //createAndReadDrafts.inject(rampUsers(1).over(1.seconds)),
   ).protocols(httpProtocol)
 }
