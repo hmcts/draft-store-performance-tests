68abcc334c06d06a9d8aeb1dcd8be38e05e6b8cf
Luigi Bitonti <luigi.bitonti@hmcts.net>
2019-04-17 14:57:48 +0100

    Static update

--- src/gatling/scala/uk/gov/hmcts/reform/draftstore/actions/Update.scala
+++ src/gatling/scala/uk/gov/hmcts/reform/draftstore/actions/Update.scala
@@ -11,40 +11,32 @@ import scala.util.Random
 
 object Update {
 
-  private val secretFeeder =
-    Iterator.continually(
-      Map(
-        "update" -> Random.alphanumeric.take(10).mkString
-      )
-    )
-
   val update: ChainBuilder =
-    feed(secretFeeder)
-      .exec(
-        http("Update draft")
-          .put(url = "/${id}")
-          .headers(Map(
-            "ServiceAuthorization" -> "Bearer ${service_token}",
-            "Authorization" -> "Bearer ${user_token}",
-            "Secret" -> "${secret}"
-          ))
-          .body(
-            StringBody(
-              s"""
-                |{
-                |  "document": {
-                |    "a": "some updated value",
-                |    "b": "some other updated value (${update})",
-                |    "c": "yet another update",
-                |    "nested": {
-                |      "xxx": "${update}"
-                |    }
-                |  },
-                |  "type": "my updated type"
-                |}
-              """.stripMargin
-            )
+    exec(
+      http("Update draft")
+        .put(url = "/${id}")
+        .headers(Map(
+          "ServiceAuthorization" -> "Bearer ${service_token}",
+          "Authorization" -> "Bearer ${user_token}",
+          "Secret" -> "${secret}"
+        ))
+        .body(
+          StringBody(
+            s"""
+              |{
+              |  "document": {
+              |    "a": "some updated value",
+              |    "b": "some other updated value",
+              |    "c": "yet another update",
+              |    "nested": {
+              |      "xxx": "zzz"
+              |    }
+              |  },
+              |  "type": "my updated type"
+              |}
+            """.stripMargin
           )
-          .check(status.is(204))
-      )
+        )
+        .check(status.is(204))
+    )
 }
