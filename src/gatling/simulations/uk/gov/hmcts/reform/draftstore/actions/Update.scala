645831e1531cc78ab845801d9d7e32f476dea41e
luigibk <41112628+luigibk@users.noreply.github.com>
2019-05-01 15:06:51 +0100

    Draft files (#15)
    * Use json files of different sizes
    
    * Fix cast issue
    
    * Log selected file name
    
    * Move drafts. Debug log
    
    * Update logback for gatling 3
    
    * Do not add content-type header
    
    * Read files in memory before starting
    
    * Read files in memory before starting
    
    * Use stringbody
    
    * Use el body for files
    
    * Use json objects (not arrays)
    
    * It's document (not data...)
    
    * Clean up. Add type to drafts
    
    * Update drafts
    
    * Clean up

--- src/gatling/simulations/uk/gov/hmcts/reform/draftstore/actions/Update.scala
+++ src/gatling/simulations/uk/gov/hmcts/reform/draftstore/actions/Update.scala
@@ -1,42 +1,25 @@
 package uk.gov.hmcts.reform.draftstore.actions
 
-import java.util.UUID
-
 import io.gatling.core.Predef._
 import io.gatling.core.structure.ChainBuilder
 import io.gatling.http.Predef._
-
-import scala.util.Random
-
+import uk.gov.hmcts.reform.draftstore.actions.setup.Draft
 
 object Update {
 
   val update: ChainBuilder =
-    exec(
-      http("Update draft")
-        .put(url = "/${id}")
-        .headers(Map(
-          "ServiceAuthorization" -> "Bearer ${service_token}",
-          "Authorization" -> "Bearer ${user_token}",
-          "Secret" -> "${secret}"
-        ))
-        .body(
-          StringBody(
-            s"""
-              |{
-              |  "document": {
-              |    "a": "some updated value",
-              |    "b": "some other updated value",
-              |    "c": "yet another update",
-              |    "nested": {
-              |      "xxx": "zzz"
-              |    }
-              |  },
-              |  "type": "my updated type"
-              |}
-            """.stripMargin
+    feed(Draft.draftsFeeder)
+      .exec(
+        http("Update draft")
+          .put(url = "/${id}")
+          .headers(Map(
+            "ServiceAuthorization" -> "Bearer ${service_token}",
+            "Authorization" -> "Bearer ${user_token}",
+            "Secret" -> "${secret}"
+          ))
+          .body(
+            RawFileBody("${draft_file}")
           )
-        )
-        .check(status.is(204))
+          .check(status.is(204))
     )
 }
