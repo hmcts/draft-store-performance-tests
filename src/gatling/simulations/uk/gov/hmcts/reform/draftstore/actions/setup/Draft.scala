0463088314a42d6fc8c3dbe186049411d3dda4bc
Luigi Bitonti <luigi.bitonti@hmcts.net>
2019-05-01 11:02:55 +0100

    Use json files of different sizes

---
+++ src/gatling/simulations/uk/gov/hmcts/reform/draftstore/actions/setup/Draft.scala
@@ -0,0 +1,16 @@
+package uk.gov.hmcts.reform.draftstore.actions.setup
+
+import io.gatling.core.Predef._
+
+import scala.util.Random
+
+object Draft {
+
+  val drafts = List("os-copyleft.json", "os-licenses.json", "os-miscellaneous.json",
+    "os-obsolete.json", "os-osi-approved.json", "os-permissive.json",
+    "os-popular.json", "os-redundant.json", "os-retired.json",
+    "os-discouraged.json")
+    
+  val draftsFeeder = Iterator.continually(Map("draft_file" -> "drafts/" + drafts(Random.nextInt(10))))
+
+}
